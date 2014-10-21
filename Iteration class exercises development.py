27f2f98b639cea19e70b3f51f1055122690d27a6
40340 <40340@PC004139.coll.lr.local>
2014-10-21 13:03:29 +0100

    commit development

---
+++ Iteration class exercises development.py
@@ -0,0 +1,15 @@
+#Nicola Batty
+#21/10/2014
+#Iteration class exercises development
+
+def Task1():
+    n = -1
+    total = 0
+    while n < 0:
+        n = int(input("Please input a number: "))
+    for count in range(1,n+1):
+        total = total*count
+    print("The total is {0}".format(total))
+
+def Task2()
+    
